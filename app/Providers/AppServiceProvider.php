ffd6a602a374331f34ce0a4f2632440f2025127a
ahdikhfDev <ahdikahlida@gmail.com>
2025-10-28 20:45:19 +0700

    test

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\Request;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -17,17 +18,18 @@ class AppServiceProvider extends ServiceProvider
         // Force HTTPS di production
         if ($this->app->environment('production')) {
             URL::forceScheme('https');
+            
+            // Set trusted proxies untuk Vercel
+            Request::setTrustedProxies(
+                ['0.0.0.0/0'], 
+                \Illuminate\Http\Request::HEADER_X_FORWARDED_ALL
+            );
         }
 
-        // Handle proxy headers dari Vercel
+        // Handle proxy headers
         if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
             $this->app['request']->server->set('HTTPS', 'on');
             $_SERVER['HTTPS'] = 'on';
-        }
-
-        // Pastikan asset URL menggunakan HTTPS
-        if ($this->app->environment('production')) {
-            $this->app['url']->forceScheme('https');
         }
     }
 }
